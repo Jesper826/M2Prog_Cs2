0d44ceee364f54de68bc4eed0bd3ae92b4e4ec93
JespervanNiekerk <jespervanniekerk1@gmail.com>
2024-11-20 11:51:57 +0100

    update

--- 01 functions/Program.cs
+++ 01 functions/Program.cs
@@ -20,31 +20,31 @@
 
         internal void Vraag1() 
         { 
-            Console.WriteLine("");
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
             string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord); 
         }
         internal void Vraag2()
         {
-            Console.WriteLine("");
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
             string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord);
         }
         internal void Vraag3()
         {
-            Console.WriteLine("");
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
             string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord);
         }
         internal void Vraag4()
         {
-            Console.WriteLine("");
+            Console.WriteLine("What would you do if you won a million dollars?");
             string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord);
         }
         internal void Vraag5()
         {
-            Console.WriteLine("");
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
             string antwoord = Console.ReadLine();
             Console.WriteLine(antwoord);
         }
