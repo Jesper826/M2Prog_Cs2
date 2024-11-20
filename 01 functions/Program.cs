89beeedb2b15935b3c83c2da0780f29ecdf59f48
JespervanNiekerk <jespervanniekerk1@gmail.com>
2024-11-20 11:47:26 +0100

    update

--- 01 functions/Program.cs
+++ 01 functions/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); // Een object van de Program-klasse maken //variabele Program met als type program
+            program.Run();//program wordt uitgevoerd //De functie heet run
+        }
+
+        internal void Run()
+        {
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+
+        }
+
+        internal void Vraag1() 
+        { 
+            Console.WriteLine("");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord); 
+        }
+        internal void Vraag2()
+        {
+            Console.WriteLine("");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag3()
+        {
+            Console.WriteLine("");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag4()
+        {
+            Console.WriteLine("");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag5()
+        {
+            Console.WriteLine("");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
         }
     }
 }
+
+
+
