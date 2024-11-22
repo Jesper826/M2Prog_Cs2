b827a80ac7874b77077e1e1bc2319156b8ace2e4
JespervanNiekerk <jespervanniekerk1@gmail.com>
2024-11-22 10:17:42 +0100

    update

---
+++ RijSchool/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RijSchool
+{
+    internal class Student
+    {
+        internal string naam;
+        internal int leeftijd;
+        internal LesPakket lesPakket;
+        internal RijLeraar leraar;
+        internal RijTest rijTest;
+    }
+}
