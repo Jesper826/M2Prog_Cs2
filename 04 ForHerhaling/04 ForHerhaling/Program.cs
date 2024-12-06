86e1c64f08304b0130be9f3bb366bfbeebd51ac4
JespervanNiekerk <jespervanniekerk1@gmail.com>
2024-12-06 10:21:48 +0100

    input foreach

---
+++ 04 ForHerhaling/04 ForHerhaling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ArrayOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double[] prijzen = new double[] { 0.99, 5.60, 10.11, 3.90 };
+            string[] artikelen = { "snoepje", "luxe broodje", "lunch menu", "toetje" };
+            Formulier[] formulieren = new Formulier[2];
+
+            formulieren[0] = new Formulier()
+            {
+                Sterren = 5,
+                Feedback = "Lekker eten en goede service!"
+            };
+
+            formulieren[1] = new Formulier()
+            {
+                Sterren = 3,
+                Feedback = "Eten was prima, maar het duurde te lang."
+            };
+
+            for (int i = 0; i < prijzen.Length; i++)
+            {
+                Console.WriteLine(prijzen[i]);
+                Console.WriteLine(artikelen[i]);
+            }
+
+            foreach (Formulier formulier in formulieren)
+            {
+                Console.WriteLine(formulier.Sterren);
+                Console.WriteLine(formulier.Feedback);
+            }
+        }
+    }
+    internal class Formulier()
+    {
+        internal int Sterren { get; set; }
+        internal string Feedback { get; set; }
+    }
+
+}
