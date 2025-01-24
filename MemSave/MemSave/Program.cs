2e47e87b67f97b2be015f05c4d704db25579f8d2
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-01-24 10:23:17 +0100

    memsave

---
+++ MemSave/MemSave/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace MemSave
+{
+    internal class Program
+    {
+        string saveFile = "welkomState.txt";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            string welkomsTekst = "Hello bbbbborld";
+
+            bool bestaatDeFile = File.Exists(saveFile);
+            if (bestaatDeFile)
+            {
+               
+                welkomsTekst = File.ReadAllText(saveFile);
+                Console.WriteLine("nieuwe text:" + welkomsTekst);
+            }
+            
+            while (true)
+            {
+                Console.WriteLine(welkomsTekst);
+                Console.WriteLine("Enter a text, then press enter:");
+                welkomsTekst = Console.ReadLine();
+                File.WriteAllText(saveFile, welkomsTekst);
+            }
+        }
+    }
+}
