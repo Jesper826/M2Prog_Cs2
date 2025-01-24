c63a61e52405266fcdb803f85555a1ca207029f3
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-01-24 09:30:20 +0100

    files added

---
+++ FileIo/FileIo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileIo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();            
+        }
+
+        internal void Run()
+        {
+
+        }
+    
+
+    }
+}
