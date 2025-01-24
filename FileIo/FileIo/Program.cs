16b818159c4f0eb5da70dae1ab2f15db0a387b08
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-01-24 09:45:37 +0100

    NOS GEPUSHED

--- FileIo/FileIo/Program.cs
+++ FileIo/FileIo/Program.cs
@@ -10,9 +10,21 @@
 
         internal void Run()
         {
+            Console.WriteLine(Environment.CurrentDirectory);
+            string[] lines = File.ReadAllLines("quiz.txt");
+            for (int i = 0; i < lines.Length; i++) 
+            { 
+                Console.WriteLine($"{i} {lines[i]}");
+            }
 
+            Console.WriteLine(Environment.CurrentDirectory);
+            string[] lines2 = File.ReadAllLines("Nos.txt");
+            for (int i = 0; i < lines2.Length; i++)
+            {
+                Console.WriteLine($"{i} {lines2[i]}");
+            }
         }
-    
+        
 
     }
 }
